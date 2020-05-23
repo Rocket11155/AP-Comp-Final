37ad1f2209b1f788ae70d20eb5ef1cf3755b0acb
Rocket11155 <65723826+Rocket11155@users.noreply.github.com>
2020-05-23 18:33:06 -0400

    Add files via upload

--- Tower.java
+++ Tower.java
@@ -4,6 +4,7 @@ import java.awt.geom.Rectangle2D;
 public class Tower 
 {
 	private Rectangle2D.Double tower = new Rectangle2D.Double(0,0,50,50);
+	private Rectangle2D.Double test = new Rectangle2D.Double(5,5,5,5);
 	
 
 }
