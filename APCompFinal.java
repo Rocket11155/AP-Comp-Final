11262390c4cc425fc9d0d46d08cf1000053e19a2
alexwiersbitzky <65724077+alexwiersbitzky@users.noreply.github.com>
2020-05-28 21:09:40 -0400

    Add files via upload

--- APCompFinal.java
+++ APCompFinal.java
@@ -19,7 +19,12 @@ public class APCompFinal extends JFrame implements ActionListener
 	private ArrayList<lazer> list3;
 	private int score1 = 0;
 	private int score2 = 0;
-	private JLabel lblScore;
+	private JLabel topScore;
+	private JLabel btmScore;
+	private JLabel topWinner;
+	private JLabel btmWinner;
+	private JLabel instructions1;
+	private JLabel instructions2;
 	private int bulletCount = 0;
 	private boolean fired = false;
 	private boolean scored = false;
@@ -33,6 +38,50 @@ public class APCompFinal extends JFrame implements ActionListener
 		this.setBounds(500,100,400,700);
 		setResizable(false);
 		setLayout(null);
+		
+		instructions1 = new JLabel("W key - Bottom Ship ");
+		instructions1.setSize(new Dimension(200,10));
+		instructions1.setLocation(150,25);
+		instructions1.setForeground(Color.white);
+		instructions1.setFont(new Font("Serif", Font.PLAIN, 15));
+		add(instructions1);
+		
+		instructions2 = new JLabel("^ Arrow key - Top Ship");
+		instructions2.setSize(new Dimension(200,10));
+		instructions2.setLocation(150,10);
+		instructions2.setForeground(Color.white);
+		instructions2.setFont(new Font("Serif", Font.PLAIN, 15));
+		add(instructions2);
+		
+		topScore = new JLabel("0");
+		topScore.setSize(new Dimension(100,100));
+		topScore.setLocation(340,100);
+		topScore.setForeground(Color.white);
+		topScore.setFont(new Font("Serif", Font.PLAIN, 50));
+		add(topScore);
+		
+		btmScore = new JLabel("0");
+		btmScore.setSize(new Dimension(100,100));
+		btmScore.setLocation(340,550);
+		btmScore.setForeground(Color.white);
+		btmScore.setFont(new Font("Serif", Font.PLAIN, 50));
+		add(btmScore);
+		
+		topWinner = new JLabel("Top Ship Wins!");
+		topWinner.setSize(new Dimension(200,100));
+		topWinner.setLocation(100,250);
+		topWinner.setForeground(Color.orange);
+		topWinner.setFont(new Font("Serif", Font.PLAIN, 30));
+		topWinner.setVisible(false);
+		add(topWinner);
+		
+		btmWinner = new JLabel("Bottom Ship Wins!");
+		btmWinner.setSize(new Dimension(250,100));
+		btmWinner.setLocation(100,250);
+		btmWinner.setForeground(Color.orange);
+		btmWinner.setFont(new Font("Serif", Font.PLAIN, 30));
+		btmWinner.setVisible(false);
+		add(btmWinner);
 		
 		Ship ship1 = new Ship(190,600);
 		add(ship1);
@@ -91,12 +140,13 @@ public class APCompFinal extends JFrame implements ActionListener
 					list.get(1).setDx(-1);
 				if(e.getKeyCode() == e.VK_RIGHT)
 					list.get(1).setDx(1);
-				if(e.getKeyCode() == e.VK_UP)
+				if(e.getKeyCode() == e.VK_UP && !fired)
 				{
 					lazer bullet2 = new lazer(list.get(1).getX()+8,70,"s2");
 					bullet2.setDy(1);
 					add(bullet2);
 					list3.add(bullet2);
+					fired = true;
 				}
 				
 			}
@@ -122,32 +172,7 @@ public class APCompFinal extends JFrame implements ActionListener
 			}
 		});
 		
-		for(lazer i: list3)
-		{
-			for(Ball j: list2)
-			{
-			
-				
-				if(i.getBounds().intersects(j.getBounds()) && i.getName() == "s1")
-				{
-					score1++;
-					System.out.println(score1);
-					list2.remove(j);
-					j.setLocation(2000,2000);
-				}
-				if(i.getBounds().intersects(j.getBounds()) && i.getName() == "s2")
-				{
-					score2++;
-				}
-				
-				
-				
-			}
-			
-			
-						
-		}
-		Timer t1 = new Timer(1,this);
+		Timer t1 = new Timer(2,this);
 		t1.start();
 		
 		setPreferredSize(new Dimension(400,700));
@@ -172,7 +197,56 @@ public class APCompFinal extends JFrame implements ActionListener
 	public void actionPerformed(ActionEvent e) 
 	{
 		
-		
+		for(lazer i : list3)
+		{
+			for(int j = list2.size()-1; j>= 0; j--)
+			{
+				
+				if(i.getBounds().intersects(list2.get(j).getBounds()) && i.getName().equals("s1"))
+				{
+					
+					score1++;
+					list2.get(j).setVisible(false);
+					list2.remove(j);
+					i.setVisible(false);
+					i.setLocation(0,0);
+					btmScore.setText("" + score1);
+					
+					if(score1 + score2 == 5 && score1 > score2)
+					{
+						btmWinner.setVisible(true);
+					}
+					if(score1 + score2 == 5 && score1 < score2)
+					{
+						topWinner.setVisible(true);
+					}
+					
+				}
+				
+				else if(i.getBounds().intersects(list2.get(j).getBounds()) && i.getName().equals("s2"))
+				{
+					score2++;
+					list2.get(j).setVisible(false);
+					list2.remove(j);
+					i.setVisible(false);
+					i.setLocation(400,700);	
+					topScore.setText("" + score2);
+					if(score1 + score2 == 5 && score1 > score2)
+					{
+						btmWinner.setVisible(true);
+					}
+					if(score1 + score2 == 5 && score1 < score2)
+					{
+						topWinner.setVisible(true);
+					}
+				}
+				
+				
+				
+			}
+			
+						
+		}
 		
 		for(Ship i:list)
 		{
